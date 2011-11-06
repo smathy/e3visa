a5d9b4cbe006f4e56621ba4f05d8d0e5a95550fd
Jason King <jasonk@amcoonline.net>
2011-11-06 15:57:05 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+E3visaMe::Application.config.secret_token = '2523c1d3d82e794238ebdf2d10922aad75f93461d56dc84060b40a03db77724087335899afed158df095a94cf87a8d0d9c7cf5e6f32616c4b1783609b9561277'
